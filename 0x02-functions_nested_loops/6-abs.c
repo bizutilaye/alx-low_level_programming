cd0a18d935e23dac5e5223102d20ec0f3230625f
bizutilaye <bizualemtilaye@gmail.com>
2022-12-13 13:19:39 -0800

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs -compute absolute value
+ * @t: the integer
+ * Return: The aabsolute value
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
