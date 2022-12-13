e0622fe3f8c26e0c0ab15eee0ec7dd6aeca60b38
bizutilaye <bizualemtilaye@gmail.com>
2022-12-13 11:23:59 -0800

    Print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+ #include "main.h"
+/**
+ * main: print_alphabet - entery point
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c = 'a')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar("\n");
+}
