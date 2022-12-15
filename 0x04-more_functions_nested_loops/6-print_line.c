34d282016fc98fec252eac7cb048681103298a4c
bizutilaye <bizualemtilaye@gmail.com>
2022-12-15 11:25:50 -0800

    Drawe line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,13 +4,13 @@
  * print_line - drawes a straight line in the terminal
  * @n: number of times for the _ line
  */
-void print_line(n)
+void print_line(int n)
 {
 	int i;
 
 	for (i = 1; i <= n; i++)
 	{
-		_putchar(95);
+		_putchar('_');
 	}
 	_putchar('\n');
 }
