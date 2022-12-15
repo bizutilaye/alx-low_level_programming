d6d1a525683f1f25a6af2dd4775eddac4189d7a2
bizutilaye <bizualemtilaye@gmail.com>
2022-12-15 10:26:30 -0800

    Print number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ * Return: no return value
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (char c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
