57528a87b46623188a1942c02b9df00a14c67d49
bizutilaye <bizualemtilaye@gmail.com>
2022-12-22 12:56:33 -0800

    Changing allcharacter in upper case

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,6 +10,9 @@ char *cap_string(char *str)
 {
 	int index = 0;
 
+	while (str[index])
+	{
+
 	while (!(str[index] >= 'a' && str[index] <= 'z'))
 		index++;
 	if (str[index - 1] == ' ' ||
@@ -28,4 +31,6 @@ char *cap_string(char *str)
 	    index == 0)
 		str[index] -= 32;
 	index++;
+	}
+	return (str);
 }
