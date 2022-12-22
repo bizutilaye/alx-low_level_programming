8a9c5ee1e94b87386ac6da1486a355a6258c6e2f
bizutilaye <bizualemtilaye@gmail.com>
2022-12-22 11:44:05 -0800

    concatenate string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strncat - This function appends the src string to the dest sring it will use at most n bytes from src
+ * _strncat - This function appends the src string to the dest
  * @dest: the string to be uppend on
  * @src: the string to be appendto dest
  * @n: the number of bytes from src
@@ -11,14 +11,8 @@ char *_strncat(char *dest, char *src, int n)
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
-	{
 		dest_len++;
-	}
-
 	for (index = 0; src[index] && index < n; index++)
-	{
 		dest[dest_len++] = src[index];
-	}
-
 	return (dest);
 }
