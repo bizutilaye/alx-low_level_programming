c58a3f62078cc247a1022d3ea6edffa902958f1c
bizutilaye <bizualemtilaye@gmail.com>
2022-12-22 11:10:45 -0800

    concatenate string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * strcat - concatenates strings
+ * @dest: A pointer to the string to be concatenated up on
+ * @src: The source string to be append
+ * Return: Aconcatenated String
+ */
+char *_strcat(char *dest, char *src);
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+	{
+		dest_len++;
+	}
+
+	for (index = 0; src[index]; index++)
+	{
+		dest[dest_len++] = src[index];
+	}
+
+	return (dest);
+}
