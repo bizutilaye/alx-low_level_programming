27a589ba422e638dd90769fb5fb967469fcdc720
bizutilaye <bizualemtilaye@gmail.com>
2023-01-02 10:59:32 -0800

    Finding a character from a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strchr - value and string
+ * @s: input
+ * @c: input
+ * Return: o or value
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i])
+	}
+	return (0);
+}
