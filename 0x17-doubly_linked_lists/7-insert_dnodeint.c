31c8595d2ee5ff92535d7ea2e08e42893bf6d394
bizutilaye <bizualemtilaye@gmail.com>
2023-03-16 05:07:23 -0700

    Update

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,47 +8,47 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *new_node, *current;
-    unsigned int i;
+	dlistint_t *new_node, *current;
+	unsigned int i;
 
-    if (h == NULL) /* Check for NULL head pointer */
-        return (NULL);
+	if (h == NULL) /* Check for NULL head pointer */
+	return (NULL);
+i
+	/* Create a new node */
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+	return (NULL);
+	new_node->n = n;
 
-    /* Create a new node */
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return (NULL);
-    new_node->n = n;
+	/* Handle special case when idx == 0 */
+	if (idx == 0)
+	{
+	new_node->prev = NULL;
+	new_node->next = *h;
+	if (*h != NULL)
+		(*h)->prev = new_node;
+	*h = new_node;
+	return (new_node);
+	}
 
-    /* Handle special case when idx == 0 */
-    if (idx == 0)
-    {
-        new_node->prev = NULL;
-        new_node->next = *h;
-        if (*h != NULL)
-            (*h)->prev = new_node;
-        *h = new_node;
-        return (new_node);
-    }
+	/* Traverse the list to be inserted */
+	current = *h;
+	for (i = 0; i < idx - 1 && current != NULL; i++)
+	current = current->next;
 
-    /* Traverse the list to the position where the new node should be inserted */
-    current = *h;
-    for (i = 0; i < idx - 1 && current != NULL; i++)
-        current = current->next;
+	/* If current is NULL*/
+	if (current == NULL)
+	{
+	free(new_node);
+	return (NULL);
+	}
 
-    /* If current is NULL, we have reached the end of the list and cannot insert */
-    if (current == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	/* Insert the new node */
+	new_node->prev = current;
+	new_node->next = current->next;
+	if (current->next != NULL)
+		current->next->prev = new_node;
+	current->next = new_node;
 
-    /* Insert the new node */
-    new_node->prev = current;
-    new_node->next = current->next;
-    if (current->next != NULL)
-        current->next->prev = new_node;
-    current->next = new_node;
-
-    return (new_node);
+	return (new_node);
 }
